b46666e7826b21ccd44955d94ca43b42cfd00fa0
Darin Hunt <dhunt10@gmail.com>
2020-04-07 10:10:52 -0400

    added ability for multiple stocks

--- getter.py
+++ getter.py
@@ -9,15 +9,21 @@ def quoteGetter(stock):
     """
 
     """
-    URL = 'https://finance.yahoo.com/quote/{}?p={}'.format(stock, stock)
-    prev = 0
-    value = 0
-    try:
-         r = requests.get(URL)
-         soup = bs4.BeautifulSoup(r.text, 'html.parser')
-         prev = value
-         value = float(soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text.encode('ascii', 'ignore'))
-    except(ValueError):
-        print(bcolors.FAIL + 'Stock is not yet acceptable')
-
-    return (value)
+    final = ""
+    stock_list = stock.split()
+    for item in stock_list:
+        print("ITEM: {}".format(item))
+        URL = 'https://finance.yahoo.com/quote/{}?p={}'.format(item, item)
+        prev = 0
+        value = 0
+        try:
+             r = requests.get(URL)
+             soup = bs4.BeautifulSoup(r.text, 'html.parser')
+             prev = value
+             value = float(soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text.encode('ascii', 'ignore'))
+        except(ValueError):
+            print(bcolors.FAIL + 'Stock is not yet acceptable')
+        except(IndexError):
+            print("Out of Data")
+        final = final + item + ": " + str(value) + " "
+    return (final)
